ad04e46e285a38ca29b7f6bdcc8d1157c3bbdf39
Timothy Dao <timster78811@gmail.com>
2018-10-29 17:08:54 -0400

    added test

---
+++ test/server.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Clear the console before each run
+// process.stdout.write("\x1Bc\n");
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Reality Check', () => {
+  it('true should be true', () => {
+    expect(true).to.be.true;
+  });
+
+  it('2 + 2 should equal 4', () => {
+    expect(2 + 2).to.equal(4);
+  });
+});
