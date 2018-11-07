277954030af520354a934b201ef3c0f01db469c3
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-06 21:05:20 -0500

    more file additions and answers for javascript

--- validators/evaluations/javascript/answer1.js
+++ validators/evaluations/javascript/answer1.js
@@ -2,11 +2,10 @@
 const {evaluateFn} = require('../../helpers/javascript/javascript-helper1');
 
 module.exports = (jsString, res) => {
-  try{  
-    evaluateFn(jsString,  );
-    
+  try {  
+    evaluateFn(jsString, {a: 1, b: 2}, 1);
     res.json({error: false, message: 'Challenge completed'});
-  }catch(e){
+  } catch(e) {
     console.log(e);
     res.json({error: true, message: e});
   }
