35b671f1f093b35b482c6e8c7b8332d83d5a5397
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-09 15:21:36 -0500

    pushing working version of validation.  question 3,4,5 of javascript still need to be fixed

--- validators/helpers/javascript/javascript-helper1.js
+++ validators/helpers/javascript/javascript-helper1.js
@@ -5,15 +5,15 @@ const mongoose = require('mongoose');
 const UserStats = require('../../../models/userStats');
 
 module.exports = {
-  evaluateFn: function(answer, input, output, res, userId, next) {
+  evaluateFn: function(answer, input, output) {
     const values = Object.values(input);
     
     const cleanAnswer = `"use strict";return(${answer})`;
     const fn = Function(cleanAnswer)();
-    if ((fn(...values)) === undefined) {
-      return 'incorrect';
-    } else {
-      expect(fn(...values)).to.eql(output);
-    }
+    // if ((fn(...values)) === undefined) {
+    //   return 'incorrect';
+    // } else {
+    expect(fn(...values)).to.eql(output);
+    // }
   }
 };
